f9e81962a54883e85cfe1156bcb99fec541fedb3
yeonhwa001 <chyh202@naver.com>
2024-06-25 15:11:08 +0900

    .

--- src/main/java/org/choongang/member/controllers/MemberController.java
+++ src/main/java/org/choongang/member/controllers/MemberController.java
@@ -1,48 +1,39 @@
 package org.choongang.member.controllers;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import lombok.RequiredArgsConstructor;
-import org.choongang.global.config.annotations.*;
-import org.choongang.member.services.JoinService;
+import org.choongang.global.config.annotations.Controller;
+import org.choongang.global.config.annotations.GetMapping;
+import org.choongang.global.config.annotations.PostMapping;
+import org.choongang.global.config.annotations.RequestMapping;
 
 @Controller
 @RequestMapping("/member")
-@RequiredArgsConstructor
 public class MemberController {
-    private final JoinService joinService;
 
-    @GetMapping
-    public String index(HttpServletRequest request) {
-        String attr = (String) request.getAttribute("commonValue");
-       // System.out.println(attr);
-        /*
-        boolean bool = true;
-        if (bool) {
-            throw new RuntimeException("테스트1212121212");
-        }
+    // 회원 가입 양식
+    @GetMapping("/join")
+    public String join() {
 
-         */
-        return "member/index";
-    }
-
-    @GetMapping("/{mode}/test/{num}")
-    public String join(@PathVariable("mode") String mode, @RequestParam("seq") int seq, RequestJoin form,  HttpServletResponse response, @PathVariable("num") int num) {
-        System.out.printf("mode=%s, seq=%d, num=%d%n", mode, seq, num);
-        System.out.println(form);
-        joinService.process();
         return "member/join";
     }
 
-    /*
-    @ExceptionHandler({RuntimeException.class})
-    public String errorHandler(RuntimeException e1, HttpServletRequest request, HttpServletResponse response, HttpSession session) {
-        System.out.println(e1);
-        System.out.println(e2);
-        System.out.println(request);
-        System.out.println(response);
-        System.out.println(session);
+    // 회원 가입 처리
+    @PostMapping("/join")
+    public String joinPs() {
 
-        return "errors/error";
-    } */
+        return null;
+    }
+
+    // 로그인 양식
+    @GetMapping("/login")
+    public String login() {
+
+        return "member/login";
+    }
+
+    // 로그인 처리
+    @PostMapping("/login")
+    public String loginPs() {
+
+        return null;
+    }
 }
